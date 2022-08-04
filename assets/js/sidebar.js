efd1b27e3884a62355b43cc5344a6a78db109ec9
Vincent Hong <vinlegend1@gmail.com>
2022-08-04 09:44:44 +0800

    sidebar styling

---
+++ assets/js/sidebar.js
@@ -0,0 +1,61 @@
+const sidebarParent = document.querySelector(
+    "table:nth-child(5) tbody tr td table tbody tr:nth-child(3) td table tbody tr td:nth-child(1)"
+);
+
+sidebarParent.classList.add("sidebar__parent");
+const sidebar = sidebarParent.querySelector("table");
+sidebar.classList.add("sidebar");
+
+/** Add Icons to Sidebar Menu Items */
+const menuItemToIcon = {
+    "Change Password": `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M80 192V144C80 64.47 144.5 0 224 0C303.5 0 368 64.47 368 144V192H384C419.3 192 448 220.7 448 256V448C448 483.3 419.3 512 384 512H64C28.65 512 0 483.3 0 448V256C0 220.7 28.65 192 64 192H80zM144 192H304V144C304 99.82 268.2 64 224 64C179.8 64 144 99.82 144 144V192z"/></svg>`,
+    "Log Out": `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M160 416H96c-17.67 0-32-14.33-32-32V128c0-17.67 14.33-32 32-32h64c17.67 0 32-14.33 32-32S177.7 32 160 32H96C42.98 32 0 74.98 0 128v256c0 53.02 42.98 96 96 96h64c17.67 0 32-14.33 32-32S177.7 416 160 416zM502.6 233.4l-128-128c-12.51-12.51-32.76-12.49-45.25 0c-12.5 12.5-12.5 32.75 0 45.25L402.8 224H192C174.3 224 160 238.3 160 256s14.31 32 32 32h210.8l-73.38 73.38c-12.5 12.5-12.5 32.75 0 45.25s32.75 12.5 45.25 0l128-128C515.1 266.1 515.1 245.9 502.6 233.4z"/></svg>`,
+    "Manage Email - Google": `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><!--! Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"/></svg>`,
+    "Review Student Data": `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z"/></svg>`,
+    "View Clearance": `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M560 448H512V113.5c0-27.25-21.5-49.5-48-49.5L352 64.01V128h96V512h112c8.875 0 16-7.125 16-15.1v-31.1C576 455.1 568.9 448 560 448zM280.3 1.007l-192 49.75C73.1 54.51 64 67.76 64 82.88V448H16c-8.875 0-16 7.125-16 15.1v31.1C0 504.9 7.125 512 16 512H320V33.13C320 11.63 300.5-4.243 280.3 1.007zM232 288c-13.25 0-24-14.37-24-31.1c0-17.62 10.75-31.1 24-31.1S256 238.4 256 256C256 273.6 245.3 288 232 288z"/></svg>`,
+    "View Curriculum Audit": ``,
+    "View Visa Status (For IS)": ``,
+    "View Final Exam and Grade Consultation Schedule": ``,
+    "Print E.A.F.": ``,
+    "Reserve Adjustment Pass": ``,
+    "View Assessment (with Online Payment)": ``,
+    "View Grades": ``,
+    "Apply to Graduate": ``,
+    "Register LEAP": ``,
+    "Cast SC Plebiscite Vote": ``,
+    "Calculate Tuition and Fees": ``,
+    "Evaluate Administrator/ASF/APSP/CAP": ``,
+    "Evaluate Faculty": ``,
+    "Nominate Outstanding Professor": ``,
+    "Request for Documents": ``,
+    "Reserve Tickets": ``,
+    "Set PHEx Appointment (Student)": ``,
+    "Subscribe to Green & White": ``,
+    "View Parent's Access": ``,
+    "Change PIN": ``,
+    "Inquire Balance": ``,
+    "View Statement": ``,
+    "Pahiram Libro: Reserve Book": ``,
+};
+
+/** Remove the border lines separating each element */
+const sidebarBody = sidebarParent.querySelector("tbody");
+const sidebarChildren = sidebarBody.children;
+for (let i = 0; i < sidebarChildren.length; i++) {
+    if (sidebarChildren[i].querySelector("img")) {
+        sidebarChildren[i].remove();
+    } else {
+        sidebarChildren[i].classList.add("sidebar__main");
+    }
+
+    const subItem = sidebarChildren[i].querySelector("a");
+    if (
+        subItem &&
+        subItem.textContent &&
+        menuItemToIcon[subItem.textContent] !== ""
+    ) {
+        // icon.src = menuItemToIcon[subItem.textContent];
+        subItem.innerHTML =
+            menuItemToIcon[subItem.textContent] + subItem.innerHTML;
+    }
+}
